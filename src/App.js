217298cbf1aab3cb86ad767b183d470fad96da77
Viktordotcom <adric.jawad@gmail.com>
2021-11-20 15:25:06 +0100

    Change from class to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import GeneralInfo from "./components/General";
 import EducationalInfo from "./components/Education";
 import PracticalInfo from "./components/Practice";
@@ -6,72 +6,40 @@ import PersonalData from "./components/PersonalData";
 import profileImg from "./images/profile.jpg";
 import "./styles/style.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: PersonalData,
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
-  }
-  handleChange(event) {
+export default function App() {
+  const localData = JSON.parse(localStorage.getItem("localData"));
+  const [data, setData] = useState(localData || PersonalData);
+
+  function handleChange(event) {
     const { name, value } = event.target;
-    this.setState({
-      data: this.state.data.map((item) => {
+    setData(
+      data.map((item) => {
         for (let key in item) {
           key === name && (item[key] = value);
         }
         return item;
-      }),
-    });
-  }
-
-  componentDidMount() {
-    const localData = localStorage.getItem("localData");
-
-    if (localData) {
-      this.setState({ data: JSON.parse(localData) });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log("Component Did Update");
-    if (prevState.data !== this.state.data) {
-      localStorage.setItem("localData", JSON.stringify(this.state.data));
-    }
-  }
-
-  handleEdit(id) {
-    this.setState({
-      data: this.state.data.map((item) => {
-        return item.id === id ? { ...item, edit: !item.edit } : item;
-      }),
-    });
-  }
-
-  render() {
-    return (
-      <div className="main-container">
-        <img src={profileImg} alt="profile"></img>
-        <GeneralInfo
-          data={this.state.data}
-          change={this.handleChange}
-          edit={this.handleEdit}
-        />
-        <EducationalInfo
-          data={this.state.data}
-          change={this.handleChange}
-          edit={this.handleEdit}
-        />
-        <PracticalInfo
-          data={this.state.data}
-          change={this.handleChange}
-          edit={this.handleEdit}
-        />
-      </div>
+      })
     );
   }
-}
 
-export default App;
+  useEffect(() => {
+    localStorage.setItem("localData", JSON.stringify(data));
+  }, [data]);
+
+  function handleEdit(id) {
+    setData(
+      data.map((item) => {
+        return item.id === id ? { ...item, edit: !item.edit } : item;
+      })
+    );
+  }
+
+  return (
+    <div className="main-container">
+      <img src={profileImg} alt="profile"></img>
+      <GeneralInfo data={data} change={handleChange} edit={handleEdit} />
+      <EducationalInfo data={data} change={handleChange} edit={handleEdit} />
+      <PracticalInfo data={data} change={handleChange} edit={handleEdit} />
+    </div>
+  );
+}
